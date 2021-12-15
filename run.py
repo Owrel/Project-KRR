a7f08c07dc17daf0dcc3bdd1c22baa9e7e5a814a
Owrel <50216219+Owrel@users.noreply.github.com>
2021-12-15 16:56:08 +0100

    improving solver + fixing bugs + adding convertion + adding instances

--- run.py
+++ run.py
@@ -1,41 +1,101 @@
-### IMPORT SECTION
+# IMPORT SECTION
 import clingo
 import json
+import os
 
+#Reseting path
+reseting = False
 
 # Getting instances
-instances = ['instance01']
+instances = ['instance03']
+merger = 'graphMerger.lp'
+pathfinder = 'path.lp'
 
 
 # Setiing instances path
 path_instances = 'instances/'
-#setting plan generation
+# setting plan generation6
 path_plan = 'plans/'
+
+pathfinding = "pathfinding/"
+
+path_mergers = 'mergers/'
 
 
 file_extensions = '.lp'
 
 for instance in instances:
-    print('Instance ' + instance )
+    print('Instance ' + instance)
     f = open(path_instances+instance+file_extensions, "r")
     program = f.read()
 
-    f = open("solver.lp", "r")
-    program += f.read()
+    print('Computing simple path finding...')
 
+    if os.path.isfile(path_instances+instance+".path") and not reseting:
+        print('Reading from old computed path.')
+        f = open(path_instances+instance+".path", "r")
+        path = f.read()
+        pathfinding_time = "Imported"
+    else:
+
+        f = open(pathfinding + pathfinder, "r")
+        program += f.read()
+
+        ctl = clingo.Control()
+        ctl.add('base', [], program)
+        ctl.ground([("base", [])])
+
+        result = []
+        ctl.solve(on_model=lambda m: result.append((("{}".format(m)))))
+
+        pathfinding_time = str(ctl.statistics['summary']['times']['total'])
+
+        if result:
+
+            path = result[-1].replace(' ', '. ')
+            path += '.'
+            f = open(path_instances+instance+".path", "w")
+            f.write(path)
+            f.close()
+
+            print('Done ')
+        else:
+            print('PATHFINDING UNSATISFIABLE')
+            break
+
+    print('Merging...')
+
+    f = open(path_instances+instance+file_extensions, "r")
+    instance_program = f.read()
+
+    f = open(path_mergers+merger, "r")
+    merger_program = f.read()
 
     ctl = clingo.Control()
-    ctl.add('base',[],program)
+    ctl.add('base', [], instance_program)
+    ctl.add('base', [], merger_program)
+    ctl.add('base', [], path)
+
     ctl.ground([("base", [])])
 
     result = []
     ctl.solve(on_model=lambda m: result.append((("{}".format(m)))))
 
-
-    print(result)
+    if result:
+        print(result[-1])
+        out = result[-1].replace(' ', '. ')
+        out += '.'
+        f = open("instances/lastout.lp", "w")
+        f.write(out)
+        f.close()
+    else:
+        print('UNSATISFIABLE')
+    # print(json.dumps(ctl.statistics,indent=4))
     print('####### Additional informations #######')
-    print("Solved in " + str(ctl.statistics['summary']['times']['total']) + ' sec.')
+    print("Solved in " +
+          str(ctl.statistics['summary']['times']['total']) + ' sec.')
     print('Atoms : ' + str(ctl.statistics['problem']['lp']['atoms']))
     print('Rules : ' + str(ctl.statistics['problem']['lp']['atoms']))
+    print('Pathfinding : ' + pathfinding_time)
 
     # print(json.dumps(ctl.statistics, indent=4))
